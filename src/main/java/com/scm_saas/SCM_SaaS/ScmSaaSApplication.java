9f4b9e882d1852ad56ab37c34bb1f9b2c5f00170
devrevo <ayoub.mafkoud@gmail.com>
2024-12-29 17:02:16 +0100

    Initializing SCM-SaaS Project

---
+++ src/main/java/com/scm_saas/SCM_SaaS/ScmSaaSApplication.java
@@ -0,0 +1,14 @@
+// File: src/main/java/com/scm_saas/SCM_SaaS/ScmSaaSApplication.java
+
+package com.scm_saas.SCM_SaaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmSaaSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScmSaaSApplication.class, args);
+    }
+}
